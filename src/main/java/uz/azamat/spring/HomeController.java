812c8c567a0c206797c5cb6bf9f4beb3889c74ea
Azamat <azamatmaqsudjonovich@gmail.com>
2020-07-18 23:03:21 +0500

    Initial commit

---
+++ src/main/java/uz/azamat/spring/HomeController.java
@@ -0,0 +1,44 @@
+package uz.azamat.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+@Controller
+public class HomeController {
+    @GetMapping()
+    public String display() {
+        return "index";
+    }
+
+    @PostMapping("/upload")
+    public void singleFileUpload(@RequestParam("file") MultipartFile file, HttpServletResponse response) throws IOException {
+        ZipOutputStream zipOut = new ZipOutputStream(response.getOutputStream());
+        String originalFilename = file.getOriginalFilename();
+        assert originalFilename != null;
+        String substring = originalFilename.substring(0, originalFilename.length() - 4);
+        response.setHeader("Content-disposition", "attachment; filename=" + substring + ".zip");
+        ZipEntry zipEntry = new ZipEntry(originalFilename);
+        zipOut.putNextEntry(zipEntry);
+
+        InputStream in = file.getInputStream();
+        while (in.available() > 0) {
+            int i = in.read();
+            zipOut.write(i);
+        }
+        in.close();
+        zipOut.closeEntry();
+        zipOut.close();
+    }
+}
